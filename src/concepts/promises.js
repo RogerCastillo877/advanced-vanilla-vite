6d562119d25524122a986cb66168d675f42b6ec7
RogerCastillo877 <roger.castillo877@gmail.com>
2023-01-06 19:03:03 -0500

    Promise hell

--- src/concepts/promises.js
+++ src/concepts/promises.js
@@ -10,6 +10,13 @@ export const promisesComponent = (element) => {
     element.innerHTML = hero.name;
   })
 
+  const renderTwoHeroes = (hero1, hero2) => {
+    element.innerHTML = `
+      <h3>${hero1.name}</h3>
+      <h3>${hero2.name}</h3>
+    `;
+  }
+
   const renderError = (error) => {
     element.innerHTML = `
       <h3>${error}</h3>
@@ -20,8 +27,15 @@ export const promisesComponent = (element) => {
   const id2 = '5d86371fd55e2e2a30fe1cc4';
 
   findHero(id1)
-    .then(renderHero)
-    .catch(renderError)
+    .then(hero1 => {
+
+      findHero(id2)
+        .then(hero2 => {
+          renderTwoHeroes(hero1, hero2);
+        })
+        .catch(renderError);
+    })
+    .catch(renderError);
 }
 
 /**
